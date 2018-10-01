de18367cca1f7de35f1bada74357c92c28d19b06
Nick Phillips <nicktgphillips@gmail.com>
2018-10-01 20:32:41 +0100

    Include public folder to be served by express app

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'frontend/build')))
+app.use(express.static(path.join(__dirname, 'frontend/public')))
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
