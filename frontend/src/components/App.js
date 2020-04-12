123425bd4238404f45154d1c86e53bcb028aeede
Nick Phillips <nicktgphillips@gmail.com>
2020-04-12 17:10:35 +0100

    Update dependencies

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,10 +14,10 @@ class App extends React.Component {
         <div className={styles.header}>
           <h1>Heroku Express Webpack React Starter</h1>
           <div className={styles.icons}>
-            <HerokuIcon className={styles.icon} />
-            <ExpressIcon className={styles.icon} />
-            <WebpackIcon className={styles.icon} />
-            <ReactIcon className={styles.icon} />
+            <HerokuIcon width={48} height={48} className={styles.icon} />
+            <ExpressIcon width={48} height={48} className={styles.icon} />
+            <WebpackIcon width={48} height={48} className={styles.icon} />
+            <ReactIcon width={48} height={48} className={styles.icon} />
           </div>
         </div>
         <div className={styles.inner}>
