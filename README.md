9d207efcec5ee126c7b7de6fe88881f30926c6f2
Nick Phillips <nicktgphillips@gmail.com>
2020-04-13 10:30:39 +0100

    Added TypeScript icon, updated Readme

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-### heroku-express-webpack-react-starter
+# heroku-express-webpack-react-starter
 
-To run locally:
+This codebase is a skeleton app, intended for anyone who wants to avoid boilerplate code setup when launching a modern Node.js React app on Heroku.
+
+Main technologies used are as follows:
+- Express
+- Webpack
+- React
+- TypeScript
+- Heroku
+
+### Local Development
 
 1. [server terminal] `make server`
 2. [client teriminal] `make client`
 3. Go to localhost:3000 in your browser
 
-To run on Heroku:
+### Deploy to Heroku
 
 1. Ensure you have the [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli) installed (and a Heroku account!)
 2. Ensure you are logged in to Heroku by typing `heroku login` in a terminal
--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -5,6 +5,7 @@ import HerokuIcon from 'img/heroku.svg'
 import ExpressIcon from 'img/nodejs.svg'
 import WebpackIcon from 'img/webpack.svg'
 import ReactIcon from 'img/react.svg'
+import TypeScriptIcon from 'img/typescript.svg'
 
 
 const App: React.FunctionComponent = () => (
@@ -16,10 +17,14 @@ const App: React.FunctionComponent = () => (
         <ExpressIcon width={48} height={48} className={styles.icon} />
         <WebpackIcon width={48} height={48} className={styles.icon} />
         <ReactIcon width={48} height={48} className={styles.icon} />
+        <TypeScriptIcon width={48} height={48} className={styles.icon} />
       </div>
     </div>
     <div className={styles.inner}>
       <p>If you're seeing this, and the icons above, then congrats! It works!</p>
+    </div>
+    <div className={styles.footer}>
+      <p>Created and maintained by <a href="https://n1ck.dev/" target="_blank">Nick Phillips</a></p>
     </div>
   </div>
 )
